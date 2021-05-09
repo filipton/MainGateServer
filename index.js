44ffe5ec3c0766ac56259e2fbf0b3e375d3d8627
filipton <filipton12@gmail.com>
2021-05-09 21:31:32 +0200

    Test

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var sec_key = process.env.secret;
 var http = require('http'); http.createServer(function (req, res) { 
   var url = req.url.replace("/", "");
   var args = url.split("/");
-  res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Ok!");
+  res.writeHead(200, {'Content-Type': 'text/html'}); res.write(req.url);
   res.end();
 }).listen(process.env.PORT);
