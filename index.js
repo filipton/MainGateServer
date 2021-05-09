f68d56d183eba9cfed99a54e213d7c5e5479b929
filipton <filipton12@gmail.com>
2021-05-09 23:09:28 +0200

    Working

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ MakeGateAction = false;
 const request = require('request');
 const http = require('http'); 
 
-console.log("STARTING...");
-
 http.createServer(function (req, res) { 
   var url = req.url.substring(1);
   var args = url.split("/");
-
-  console.log(url);
 
   if(args.length == 1 && args[0] == "info"){
     res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Ok!"); 
