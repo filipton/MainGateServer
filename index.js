9abc3bfbfb15a1707e983293755e4bf3c3ca9c33
filipton <37213766+filipton@users.noreply.github.com>
2021-05-22 09:07:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,46 +24,19 @@ http.createServer(function (req, res) {
     res.end();
   }
   else if(args.length == 2 && args[0] == "gate"){
-    request(`http://51.83.133.89:21371/_token/NiktNieMozeTegoUkrasc/gate/${args[1]}`, function (error, response, body) {
-      if(error || body == ""){
-        request(`http://51.83.133.89:21372/_token/NiktNieMozeTegoUkrasc/gate/${args[1]}`, function (error2, response2, body2) {
-          if(error2 || body2 == ""){
-            request(`http://51.83.133.89:21373/_token/NiktNieMozeTegoUkrasc/gate/${args[1]}`, function (error3, response3, body3) {
-              if(error3 || body3 == ""){
-                if(args[1] == sec_key){
-                  if(MakeGateAction == false){
-                    MakeGateAction = true;
-                    res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Otwieranie bramy... (System 4)"); 
-                  }
-                  else{
-                    res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Poczekaj na zakończenie poprzedniego zapytania... (System 4)"); 
-                  }
-                }
-                else{
-                  res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Zły klucz dostępu... (System 4)"); 
-                }
-                res.end();
-              }
-              else{
-                res.writeHead(200, {'Content-Type': 'text/html'}); res.write(body3); 
-                res.end();
-                console.log("S3: " + body3);
-              }
-            });
-          }
-          else{
-            res.writeHead(200, {'Content-Type': 'text/html'}); res.write(body2); 
-            res.end();
-            console.log("S2: " + body2);
-          }
-        });
+    if(args[1] == sec_key){
+      if(MakeGateAction == false){
+        MakeGateAction = true;
+        res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Otwieranie bramy... (System 4)"); 
       }
       else{
-        res.writeHead(200, {'Content-Type': 'text/html'}); res.write(body);
-        res.end(); 
-        console.log("S1: " + body);
+        res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Poczekaj na zakończenie poprzedniego zapytania... (System 4)"); 
       }
-    });
+    }
+    else{
+      res.writeHead(200, {'Content-Type': 'text/html'}); res.write("Zły klucz dostępu... (System 4)"); 
+    }
+    res.end();
   }
   else{
     res.writeHead(200, {'Content-Type': 'text/html'}); res.write("BAD REQUEST");
