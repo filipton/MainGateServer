02ca3540fbdd3ee85b3f339168d428da23843898
filipton <37213766+filipton@users.noreply.github.com>
2021-10-03 14:33:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,46 +11,51 @@ var server = http.createServer(function (req, res) {
     body += chunk;
   });
   req.on("end", function(){
-    if(req.url.includes("/test/"))
+    if(req.url.includes("/check/"))
     {
-      var tuid = req.url.replace("/test/", "");
+      var tuid = req.url.replace("/check/", "");
       if(LastHB.has(tuid)){
         if(Date.now() - LastHB.get(tuid) < 45000 && Clients.has(tuid)){
-          res.statusCode = 200;
           res.write('true');
+          res.end();
+          return;
+        }
+      }
+      res.statusCode = 404;
+      res.write('false');
+      res.end();
+    }
+    else if(req.url.includes("/gate/"))
+    {
+      const args = req.url.replace("/gate/", "").split('/');
+      if(args.length == 2){
+        if(Clients.has(args[0])){
+          Clients.get(args[0]).send("GATE;" + args[1]);
+          res.write('Otwieranie bramy...');
           res.end();
         }
         else{
           res.statusCode = 404;
-          res.write('false');
+          res.write('Brama nie jest dostepna!');
           res.end();
         }
       }
       else{
-        res.statusCode = 404;
-        res.write('false');
+        res.statusCode = 400;
+        res.write('Zly request! /gate/{device_id}/{key}');
         res.end();
       }
-    }
-    else if(req.url.includes("/gate/"))
-    {
-      var tuid = req.url.replace("/gate/", "");
-      if(Clients.has(tuid)){
-        Clients.get(tuid).send("GATE;xyz");
-      }
-      res.write('OPENING');
-      res.end();
     }
     else if(req.url.includes("/update/"))
     {
       var tuid = req.url.replace("/update/", "");
       if(Clients.has(tuid) && body != ""){
         Clients.get(tuid).send("UPDATE;" + body);
-        res.write('UPDATING...' + "UPDATE;" + body);
+        res.write('UPDATING FROM URL: ' + body);
         res.end();
       }
       else if(body == "") {
-        res.write('Body not specified!');
+        res.write('Url not specified! (text/plain body)');
         res.end();
       }
       else{
@@ -59,7 +64,7 @@ var server = http.createServer(function (req, res) {
       }
     }
     else{
-      res.write('Hello World!');
+      res.write('Api for gate system!');
       res.end();
     }
   });
@@ -82,7 +87,6 @@ wss.on("connection", function(ws) {
       if(args.length == 2){
         Clients.set(args[1], ws);
         uid = args[1];
-        console.log(msg);
         LastHB.set(uid, Date.now());
 
         id = setInterval(function() {
